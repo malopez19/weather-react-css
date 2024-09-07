cb9941395897188e4b1fad6fac93ab557230ceeb
mjuradol <lopezmateojurado@gmail.com>
2024-09-06 20:09:27 -0500

    validation with zod or valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
+import { object, string, number, InferOutput, parse } from "valibot"
 import { SearchType, Weather } from "../types"
+//import { z } from "zod"
 
 //type guard
 /* function isWeatherResponse(weather: unknown): weather is Weather{
@@ -12,6 +14,28 @@ import { SearchType, Weather } from "../types"
         typeof (weather as Weather).main.temp_min === 'number'
     )
 } */
+
+//zod
+/* const WeatherSchema = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })    
+})
+type Weather = z.infer<typeof WeatherSchema> */
+
+//valibot
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number(),
+    })
+})
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -27,16 +51,35 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-            //Castear el type
+            //CAST TYPE
             /* const { data:  weatherData } = await axios.get<Weather>(weatherUrl)
             console.log(weatherData.name) */
 
-            //type guard o assertion
+            //TYPE GUARD O ASSERTION
             /* const { data:  weatherData } = await axios.get(weatherUrl)
             const result = isWeatherResponse(weatherData)
             if (result) {
                 console.log(weatherData.name)
             } */
+
+            //ZOD
+            /* const { data:  weatherData } = await axios.get(weatherUrl)
+            const result = WeatherSchema.safeParse(weatherData)
+            if(result.success){
+                console.log(result.data.name)
+                console.log(result.data.main.temp)
+            }else{
+                console.log('Respuesta mal formada')
+            } */
+
+            //VALIBOT
+            const { data:  weatherData } = await axios.get(weatherUrl)
+            const result = parse(WeatherSchema ,weatherData)
+            if(result){
+                console.log(result.name)
+                console.log(result.main.temp)
+            }
+
         } catch (error) {
             console.error(error)
         }
